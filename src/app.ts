f4c57a75e45fefa774f3d33a52e485960d61d865
tasniaarhamarham <tasniaarhamarham@gmail.com>
2025-12-07 22:31:15 +0600

    position changes into app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,37 @@
-import type { Request, Response } from "express"
-import express, { json, Router, urlencoded } from 'express';
+import type { Request, Response } from "express";
+import express, { json, urlencoded } from "express";
 import initDB from "./config/db";
 import route from "./routes";
 import { globalErrorHandler } from "./utils/globalError";
 import autoReturn from "./utils/autoRun";
-const app = express()
+
+const app = express();
 
 app.use(json());
-app.use(urlencoded());
-app.use('/api/v1',route);
-
+app.use(urlencoded({ extended: true }));
 
 initDB();
-app.get("/cron/auto-return", async (req, res) => {
+
+app.use("/api/v1", route);
+
+app.get("/cron/auto-return", async (req: Request, res: Response) => {
   try {
     await autoReturn();
-    res.send("Auto return job executed");
-  } catch (e) {
-    res.status(500).send("Error running job");
+    res.status(200).send("Auto return job executed");
+  } catch (e: any) {
+    console.error("AUTO RETURN ERROR:", e);
+    res.status(500).send(e.message || "Error running job");
   }
 });
 
-app.use('/', (req:Request, res:Response) => {
+app.use((req: Request, res: Response) => {
   res.status(404).json({
-    success:false,
-    message:"Not Found Route",
-    path:req.path
-  })
-})
+    success: false,
+    message: "Not Found Route",
+    path: req.path,
+  });
+});
+
 app.use(globalErrorHandler);
 
 export default app;
